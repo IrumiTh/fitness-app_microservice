1076768015d3eb931d838e8520aa3e1b76d4030f
irumi <irumiabeywickrama@gmail.com>
2025-11-06 10:27:31 +0530

    complete ai service part in rabbitmq queue listner

--- aiservice/src/main/java/com/fitness/aiservice/config/MongoConfig.java
+++ aiservice/src/main/java/com/fitness/aiservice/config/MongoConfig.java
@@ -1,4 +1,4 @@
-package com.fitness.aiservice;
+package com.fitness.aiservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
